6dd0b856f71f6c682a5ee94e2a5fe7e137086fb0
babar <babarismail@gmail.com>
2023-03-01 20:43:14 -0800

    Adding code to deploy client and server together

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
