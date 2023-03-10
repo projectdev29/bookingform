668d8d4e171edc078cc8d3d2ef89d94aa604aab4
babar <babarismail@gmail.com>
2023-03-09 23:37:40 -0800

    changing to post request

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get("/api/postpayment", (req, res) => {
   createTicket({}, "");
 });
 
-app.get("/api/pay", async (req, res) => {
+app.post("/api/pay", async (req, res) => {
   console.log("request.body: " + JSON.stringify(req.body));
   const result = await createPayment(req.body);
   res.status(200).json(result);
