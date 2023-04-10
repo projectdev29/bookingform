8b5f08cd5710809d8bbe8539e0f57d7fc7027526
babar <babarismail@gmail.com>
2023-04-10 12:07:20 -0700

    update pricing

--- server/pricing/pricinghelper.js
+++ server/pricing/pricinghelper.js
@@ -1,7 +1,7 @@
 const calculateTotal = (params) => {
   let numOfPax = [15, 15, 25, 25, 35, 35, 35, 40, 40];
   let additionalFlightLegs = [0, 0, 0, 5, 5, 5, 15, 15, 15];
-  let additionalHotels = [15, 15, 15, 20, 20, 25, 30, 35, 35];
+  let additionalHotels = [15, 15, 20, 25, 25, 30, 35, 40, 40];
   let passengerCost = numOfPax[params.passengers];
   let additionalFlightCost = additionalFlightLegs[params.flights];
   let hotelCost = params.addHotel ? additionalHotels[params.hotels] : 0;
