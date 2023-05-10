1b9fed6c013e9c80bc5b023342f3dc601d2286fe
babar <babarismail@gmail.com>
2023-05-10 00:15:58 -0700

    removing email verification for coupons

--- server/coupons/couponhelper.js
+++ server/coupons/couponhelper.js
@@ -10,11 +10,12 @@ const validateCoupon = async (coupon, email) => {
     return {
       error: "The coupon has already been used.",
     };
-  } else if (couponFromDb.email !== email) {
-    return {
-      error: "The coupon can only be applied for the assigned email address.",
-    };
   }
+  // else if (couponFromDb.email !== email) {
+  //   return {
+  //     error: "The coupon can only be applied for the assigned email address.",
+  //   };
+  // }
   // coupon is valid and can be applied
   return { isValid: true, discount: couponFromDb.discount };
 };
